2116cb8addb3aa1b59ec36b5f9728525788e7604
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-07-04 11:29:01 +0100

    Initial commit (#1)

---
+++ builder/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"path/filepath"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	src, err := filepath.Abs("../src")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	dst, err := filepath.Abs("../embed")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx := context.Background()
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	_, err = client.Pipeline("build").
+		Container().
+		From("node:16").
+		WithDirectory("/src",
+			client.Host().Directory(src, dagger.HostDirectoryOpts{
+				Include: []string{"package.json", "yarn.lock", "yarn", "vendor", ".yarnrc.yml"},
+			}),
+		).
+		WithWorkdir("/src").
+		WithExec([]string{"yarn", "install", "--immutable"}).
+		WithDirectory("/src",
+			client.Host().Directory(src),
+		).
+		WithExec([]string{"yarn", "build"}).
+		Directory("build").
+		Export(ctx, dst)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
