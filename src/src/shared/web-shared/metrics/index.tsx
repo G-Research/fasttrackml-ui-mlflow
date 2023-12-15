a01454ea8660ac6561ec4102943bef904bf8c3f8
FastTrackML CI <fasttrackml@gr-oss.io>
2023-12-15 15:13:37 +0000

    Import MLFlow UI v2.9.2

---
+++ src/src/shared/web-shared/metrics/index.tsx
@@ -0,0 +1,6 @@
+export async function recordEvent(
+  eventName: string,
+  additionalTags?: Record<string, string>,
+  eventData?: string,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+) {}
