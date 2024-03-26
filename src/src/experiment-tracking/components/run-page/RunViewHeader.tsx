0a2f8f6d97a3667f6ab62896e291369553c5ef15
fabio vincenzi <fabio.vincenzi2@studio.unibo.it>
2024-03-26 12:50:32 +0100

    add link to corrisponding run in aim

--- src/src/experiment-tracking/components/run-page/RunViewHeader.tsx
+++ src/src/experiment-tracking/components/run-page/RunViewHeader.tsx
@@ -1,11 +1,14 @@
 import { FormattedMessage } from 'react-intl';
 import { Link } from '../../../common/utils/RoutingUtils';
+import { Button } from '@databricks/design-system';
 import { OverflowMenu, PageHeader } from '../../../shared/building_blocks/PageHeader';
 import Routes from '../../routes';
 import { ExperimentEntity } from '../../types';
 import { RenameRunModal } from '../modals/RenameRunModal';
 import { RunViewModeSwitch } from './RunViewModeSwitch';
 import { shouldEnableDeepLearningUI } from '../../../common/utils/FeatureUtils';
+import { useEffect, useState } from 'react';
+import { MlflowService } from 'experiment-tracking/sdk/MlflowService';
 
 /**
  * Run details page header component, common for all page view modes
@@ -49,12 +52,26 @@ export const RunViewHeader = ({
 
   const breadcrumbs = [getExperimentPageLink()];
 
+  const [namespace, setNamespace] = useState<string>('');
+
+  useEffect(() => {
+    MlflowService.getCurrentNamespacePath().then(setNamespace);
+  }, []);
+
   return (
     <div css={{ flexShrink: 0 }}>
       <PageHeader
         title={<span data-test-id='runs-header'>{runDisplayName}</span>}
         breadcrumbs={breadcrumbs}
       >
+        <a href={`${window.location.origin}${namespace}/aim/runs/${runUuid}/overview`} target='_blank' rel='noreferrer'>
+                <Button css={{ marginLeft: 16 }} type='link' size='small'>
+                  <FormattedMessage
+                    defaultMessage='Open in Modern UI'
+                    description='Link to the corresponding experiment in the Aim UI'
+                  />
+                </Button>
+              </a>
         <OverflowMenu
           menu={[
             {
